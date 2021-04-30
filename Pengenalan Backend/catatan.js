044c63dfed4b80719b2ec754d4528c91659f7d1b
I Gede Bagus Wirawan <gusdechang24@gmail.com>
2021-04-30 22:53:15 +0800

    Pengenalan

---
+++ Pengenalan Backend/catatan.js
@@ -0,0 +1,5 @@
+/*
+GET untuk mendapatkan data, POST untuk mengirimkan data baru, 
+PUT untuk memperbarui data yang ada, dan DELETE untuk menghapus data.
+Verbs tersebutlah yang umum digunakan dalam operasi CRUD.
+*/
