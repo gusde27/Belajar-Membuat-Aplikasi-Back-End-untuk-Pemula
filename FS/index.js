d09262b05a62a82b172c1b7976b3cbbed76b6d26
I Gede Bagus Wirawan <gusdechang24@gmail.com>
2021-05-08 09:46:43 +0800

    FS Module

---
+++ FS/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+ 
+const fileReadCallback = (error, data) => {
+    if(error) {
+        console.log('Gagal membaca berkas');
+        return;
+    }
+    console.log(data);
+};
+ 
+// fs.readFile('todo.txt', 'UTF-8', fileReadCallback);
+
+const data = fs.readFileSync('note.txt', 'UTF-8', fileReadCallback);
+console.log(data);
