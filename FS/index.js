8cb8102580124e9fb12fe1268d42236672e891e1
I Gede Bagus Wirawan <gusdechang24@gmail.com>
2021-05-08 21:14:52 +0800

    add Stream File

--- FS/index.js
+++ FS/index.js
@@ -1,14 +1,32 @@
-const fs = require('fs');
+// const fs = require('fs');
  
-const fileReadCallback = (error, data) => {
-    if(error) {
-        console.log('Gagal membaca berkas');
-        return;
-    }
-    console.log(data);
-};
+// const fileReadCallback = (error, data) => {
+//     if(error) {
+//         console.log('Gagal membaca berkas');
+//         return;
+//     }
+//     console.log(data);
+// };
  
-// fs.readFile('todo.txt', 'UTF-8', fileReadCallback);
+// // fs.readFile('todo.txt', 'UTF-8', fileReadCallback);
 
-const data = fs.readFileSync('note.txt', 'UTF-8', fileReadCallback);
-console.log(data);
+// const data = fs.readFileSync('note.txt', 'UTF-8', fileReadCallback);
+// console.log(data);
+
+const fs = require('fs');
+
+const readableStream = fs.createReadStream('./article.txt', {
+    highWaterMark: 10
+});
+
+readableStream.on('readable', () => {
+    try {
+        process.stdout.write(`[${readableStream.read()}]`);
+    } catch(error) {
+        // catch the error when the chunk cannot be read.
+    }
+});
+
+readableStream.on('end', () => {
+    console.log('Done');
+});
