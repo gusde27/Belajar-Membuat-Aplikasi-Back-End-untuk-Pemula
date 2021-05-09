15f9a221ba88f227068dd510d637b0805851eab1
I Gede Bagus Wirawan <gusdechang24@gmail.com>
2021-05-09 09:59:04 +0800

    Hapi Update

---
+++ hapi-web-server/routes.js
@@ -0,0 +1,46 @@
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+    {
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            
+        }
+    },
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan';
+        },
+    },
+];
+ 
+module.exports = routes;
--- hapi-web-server/server.js
+++ hapi-web-server/server.js
@@ -1,19 +1,13 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
  
 const init = async () => {
- 
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
+        port: 5000,
+        host: 'localhost',
     });
  
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return 'Hello World!';
-        }
-    });
+    server.route(routes);
  
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
